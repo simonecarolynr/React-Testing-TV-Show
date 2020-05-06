6d82b730ecc2e575f445e08e665e874899b0761e
Simone Roy <simonecarolynr@gmail.com>
2020-05-06 13:23:29 -0600

    mvp

--- src/components/Episodes.test.js
+++ src/components/Episodes.test.js
@@ -1,4 +1,36 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import { Episodes } from './Episodes';
+import React from 'react'
+import { render } from '@testing-library/react'
+import Episodes from './Episodes'
 
+const mockEpisodes=[
+  {
+    id: 553946,
+    image: '',
+    name: 'chapter one',
+    season: 1,
+    number: 1,
+    summary: 'words',
+    runtime: 60
+  },
+  {
+    id: 553947,
+    image: '',
+    name: 'chapter two',
+    season: 2,
+    number: 2,
+    summary: 'words',
+    runtime: 60
+  }
+]
+
+test('Renders with episode data', () => {
+  const { queryAllByTestId } = render(<Episodes episodes={mockEpisodes}/>)
+  
+  expect(queryAllByTestId(/episode-list/i)).toBe([]);
+})
+
+test("Doesn't render without data", () => {
+  const { queryAllByTestId } = render(<Episodes episodes={[]}/>)
+  
+  expect(queryAllByTestId(/episode-list/i)).toHaveLength(0);
+})
